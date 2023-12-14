ee434cfc9be9b95c153419b7d275627cea946bca
Nizam Chaudhary <chaudharynizam10@gmail.com>
2023-12-14 19:23:53 +0530

    [Implemented Material Dialog to set the Wallpaper]
    Signed-off-by: Nizam Chaudhary <chaudharynizam10@gmail.com>

--- app/src/main/java/com/nizam/wallset/ui/SetWallPaperActivity.kt
+++ app/src/main/java/com/nizam/wallset/ui/SetWallPaperActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.nizam.wallset.databinding.ActivitySetWallPaperBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -40,50 +41,53 @@ class SetWallPaperActivity : AppCompatActivity() {
                 .into(binding.imageView)
         }
 
-        binding.fabSetOnBothScreen.setOnClickListener {
-           CoroutineScope(Dispatchers.Default).launch {
-               val bitmap = getBitmap()
-               val wallPaperManager = WallpaperManager.getInstance(applicationContext)
+        binding.fabApplyWallPaper.setOnClickListener {
+            val singleItems =
+                arrayOf("Set on Home Screen", "Set on Lock Screen", "Set on Both Screen")
+            var checkedItem = 2
 
-               try {
-                   wallPaperManager.setBitmap(bitmap)
-                   moveTaskToBack(true)
-               } catch (e: Exception) {
-                   e.printStackTrace()
-               }
-           }
-        }
-
-        binding.fabSetOnLockScreen.setOnClickListener {
-           CoroutineScope(Dispatchers.Default).launch {
-               val bitmap = getBitmap()
-               val wallPaperManager = WallpaperManager.getInstance(applicationContext)
-
-               try {
-                   wallPaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_LOCK)
-                   moveTaskToBack(true)
-               } catch (e: Exception) {
-                   e.printStackTrace()
-               }
-           }
-        }
-
-        binding.fabSetOnHomeScreen.setOnClickListener {
-            CoroutineScope(Dispatchers.Default).launch {
-                val bitmap = getBitmap()
-                val wallPaperManager = WallpaperManager.getInstance(applicationContext)
-
-                try {
-                    wallPaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_SYSTEM)
-                    moveTaskToBack(true)
-                } catch (e: Exception) {
-                    e.printStackTrace()
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Apply WallPaper")
+                .setNeutralButton("Cancel") { dialog, which ->
+                    dialog.dismiss()
                 }
-            }
+                .setPositiveButton("Apply") { dialog, _ ->
+                    when (checkedItem) {
+                        0 -> setWallPaper(getBitmap(), ApplyWallPaperTo.HOME_SCREEN.value)
+                        1 -> setWallPaper(getBitmap(), ApplyWallPaperTo.LOCK_SCREEN.value)
+                        2 -> setWallPaper(getBitmap(), ApplyWallPaperTo.ON_BOTH_SCREEN.value)
+                    }
+                }
+                .setSingleChoiceItems(singleItems, checkedItem) { dialog, selectedOption ->
+                    checkedItem = selectedOption
+                }
+                .show()
         }
     }
 
     private fun getBitmap(): Bitmap {
         return binding.imageView.drawable.toBitmap()
+    }
+
+    private fun setWallPaper(bitmap: Bitmap, where: Int) {
+        CoroutineScope(Dispatchers.Default).launch {
+            val wallPaperManager = WallpaperManager.getInstance(applicationContext)
+            try {
+                if (where == ApplyWallPaperTo.ON_BOTH_SCREEN.value) {
+                    wallPaperManager.setBitmap(bitmap)
+                } else {
+                    wallPaperManager.setBitmap(bitmap, null, false, where)
+                }
+                moveTaskToBack(true)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private enum class ApplyWallPaperTo(val value: Int) {
+        HOME_SCREEN(WallpaperManager.FLAG_SYSTEM),
+        LOCK_SCREEN(WallpaperManager.FLAG_LOCK),
+        ON_BOTH_SCREEN(0)
     }
 }
