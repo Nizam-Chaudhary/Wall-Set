fd9dda6adf5c06f2641fd9af6fdf32475b4274bc
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2023-12-26 19:06:50 +0530

    Refactor
    Signed-off-by: Nizam Chaudhary <ChaudharyNizam10@gmail.com>

--- app/src/main/java/com/nizam/wallset/data/database/SharedPreferences.kt
+++ app/src/main/java/com/nizam/wallset/data/database/SharedPreferences.kt
@@ -8,25 +8,4 @@ class SharedPreferences(
 ) {
     private val sharedPreferences: SharedPreferences = context.getSharedPreferences("sharedPreferences",Context.MODE_PRIVATE)
 
-    private val fetchedDayOfMonth = "FetchedDayOfMonth"
-    private val fetchMonth = "FetchedMonth"
-    private val fetchYear = "FetchedYear"
-
-    fun setLastFetchedDate(date: Array<Int>) {
-        sharedPreferences.edit().apply {
-            this.putInt(fetchedDayOfMonth,date[0])
-            this.putInt(fetchMonth,date[1])
-            this.putInt(fetchYear,date[2])
-            this.apply()
-        }
-    }
-
-    fun toLoad(date: Array<Int>) = sharedPreferences.getInt(fetchedDayOfMonth, 0) < date[0] ||
-        sharedPreferences.getInt(fetchMonth, 0) < date[1] || sharedPreferences.getInt(fetchYear, 0) < date [2]
-
-    fun getVal() = arrayOf(
-        sharedPreferences.getInt(fetchedDayOfMonth,0),
-        sharedPreferences.getInt(fetchMonth,0),
-        sharedPreferences.getInt(fetchYear,0),
-        )
 }
